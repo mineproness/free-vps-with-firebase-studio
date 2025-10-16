31bc48ea362977f4c824673bcdaf8074f0052c62
mineproness <mineproclient@gmail.com>
2025-10-16 12:19:11 +0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+rm -rf /etc/profile
+cat >> "/etc/profile" << EOF
+/etc/skel/.bashrc
+export PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin 
+export TERM=\$TERM 
+export LANG=C.UTF-8
+if [ -f \$HOME/.bashrc]; then
+  . \$HOME/.bashrc
+fi
+EOF
