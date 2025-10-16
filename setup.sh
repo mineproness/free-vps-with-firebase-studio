a6c7ad0a94d0bf906eb39bc517d39d3f8c7f5185
mineproness <mineproclient@gmail.com>
2025-10-16 13:19:57 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,4 @@
-rm -rf /etc/profile
 cat >> "/etc/profile" << EOF
-/etc/skel/.bashrc
 export PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin 
 export TERM=\$TERM 
 export LANG=C.UTF-8
