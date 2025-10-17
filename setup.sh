730b5ea333917992d14c6f9fb0352fd9c07d2ae0
mineproness <mineproclient@gmail.com>
2025-10-17 20:17:00 +0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,11 @@ cat >> "/etc/profile" << EOF
 export PATH=/usr/local/sbin:/usr/local/bin:/bin:/usr/bin:/sbin:/usr/sbin 
 export TERM=\$TERM 
 export LANG=C.UTF-8
+export HOSTNAME=ubuntu
 EOF
+cat >> "/bin/desktopstart" << EOF
+vncserver -kill :* &> /dev/null
+vncserver -xstartup startxfce4
+EOF
+chmod +x /bin/desktopstart
+
